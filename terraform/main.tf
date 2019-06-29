d160fc8798648c95faefb30da95717a882c49d95
Dmitriy Usachev <diman-110@list.ru>
2019-06-29 14:40:27 +0000

    add tag to resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,7 @@ resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-b"
+  tags = ["reddit-app"]
   # определение загрузочного диска
   boot_disk {
     initialize_params {
