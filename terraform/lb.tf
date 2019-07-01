9b10638d084e99fe14db5f0e79033684a1cb2550
Dmitriy Usachev <diman-110@list.ru>
2019-07-01 21:30:30 +0300

    move instance group from main.tf to lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,2 +1,8 @@
 # load balancer configuration
-
+# Create instanse group
+resource "google_compute_instance_group" "reddit-app" {
+  name = "reddit-app"
+  description = "Reddip app instanse group"
+  zone = "${var.zone}"
+  instances = [ "${google_compute_instance.app.self_link}" ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -93,10 +93,3 @@ resource "google_compute_project_metadata" "many_keys" {
   }
 }
 
-# Create instanse group
-resource "google_compute_instance_group" "reddit-app" {
-  name = "reddit-app"
-  description = "Reddip app instanse group"
-  zone = "${var.zone}"
-  instances = [ "${google_compute_instance.app.self_link}" ]
-}
