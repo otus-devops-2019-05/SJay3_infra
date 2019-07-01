788d7324bee56c56840c44a2524bd1ca4abe9dfc
Dmitriy Usachev <diman-110@list.ru>
2019-07-01 12:02:08 +0300

    add instance self_link to instance group

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,4 +98,5 @@ resource "google_compute_instance_group" "reddit-app" {
   name = "reddit-app"
   description = "Reddip app instanse group"
   zone = "${var.zone}"
+  instances = [ "${google_compute_instance.app.self_link}" ]
 }
