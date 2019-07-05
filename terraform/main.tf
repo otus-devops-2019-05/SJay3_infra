759c2424736ae5e594f662ab254572d682e6a073
Dmitriy Usachev <diman-110@list.ru>
2019-07-05 11:02:39 +0300

    Add second instance reddit-app2 to main.tf add external ip variable of reddit-app2

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,53 @@ resource "google_compute_instance" "app" {
   }
 }
 
+resource "google_compute_instance" "app2" {
+  name         = "reddit-app2"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  metadata {
+    # Путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  # Подключение провиженоров к ВМ
+  connection {
+    type  = "ssh"
+    user  = "appuser"
+    agent = false
+
+    # путь до приватного ключа
+    private_key = "${file("~/.ssh/appuser")}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
 
