2d7553cd6cb7bbff22e91d7a0abc7e03fc6e2a41
Dmitriy Usachev <diman-110@list.ru>
2019-07-05 14:34:42 +0000

    fix outputs.tf to use in homework 7

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,7 +6,7 @@ output "app_external_ip" {
 #   value = "${google_compute_instance.app2.network_interface.0.access_config.0.nat_ip}"
 # }
 
-output "lb_external_ip" {
-  value = "${google_compute_global_forwarding_rule.reddit-forward.ip_address}"
-}
+#output "lb_external_ip" {
+#  value = "${google_compute_global_forwarding_rule.reddit-forward.ip_address}"
+#}
 
