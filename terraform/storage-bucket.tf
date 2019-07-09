0e9bbdcd607210fc8d469ee5d43199290e820bf3
Dmitriy Usachev <diman-110@list.ru>
2019-07-09 11:01:16 +0000

    fix bucket name

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -9,7 +9,7 @@ module "storage-bucket" {
   source = "SweetOps/storage-bucket/google"
   version = "0.1.1"
   # Имена поменяйте на другие
-  name = ["SJay3-reddit-bucket-test", "SJay3-reddit-storage-bucket-test2"]
+  name = ["sjay3-reddit-bucket-test", "sjay3-reddit-storage-bucket-test2"]
 }
 
 output storage-bucket_url {
