57752aa564a9effd406d16e4d16a183affd2f1fb
Dmitriy Usachev <diman-110@list.ru>
2019-07-09 23:09:58 +0300

    fix path in module app

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -41,12 +41,12 @@ resource "google_compute_instance" "app" {
    }
 
    provisioner "file" {
-     source      = "files/puma.service"
+     source      = "${path.module}/files/puma.service"
      destination = "/tmp/puma.service"
    }
 
    provisioner "remote-exec" {
-     script = "files/deploy.sh"
+     script = "${path.module}/files/deploy.sh"
    }
 }
 
