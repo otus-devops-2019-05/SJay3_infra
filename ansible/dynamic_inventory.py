2b7c8bc150de31a782064380707752bbd50632af
Dmitriy Usachev <diman-110@list.ru>
2019-07-18 16:10:26 +0300

    Dynamic inventory script for ansible

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 
-''' inventory script for ansible
+''' dynamic inventory script
 You need to install google-api-python-client and google-cloud-storage
 pip install --upgrade google-api-python-client
 pip install google-cloud-storage
 '''
-
-import json
+import os
+import sys
+import argparse
+import re
+import warnings
 import googleapiclient.discovery
 from google.cloud import storage
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+#vars
 prefix = 'stage'
 gcp_bucket = 'sjay3-terraform-' + prefix
 bucket_folder = 'reddit-' + prefix
@@ -17,28 +26,103 @@ path_to_state = gcp_bucket + '/' + bucket_folder + '/default.tfstate'
 
 project = 'infra-244211'
 zone = 'europe-west1-b'
+warnings.filterwarnings("ignore") # ignor gcp warnings
 
-# authorize in GCP
-#compute = googleapiclient.discovery.build('compute', 'v1')
+class DynamicInventory(object):
 
-def list_instances(compute, project, zone):
-    result = compute.instances().list(project=project, zone=zone).execute()
-    return result['items'] if 'items' in result else None
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
 
-# instances = list_instances(compute, project, zone)
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.ansible_inventory()
+            with open("inventory.json", "w") as data_file:
+				json.dump(self.inventory, data_file, indent=4)
 
-# for instance in instances:
-#         print(' - ' + instance['name'])
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.empty_inventory()
+        # If no groups or vars are present, return an empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
 
-#read bucket
-client = storage.Client()
-bucket = client.get_bucket(gcp_bucket)
-blob = bucket.get_blob(bucket_folder + '/default.tfstate')
-tfstate = blob.download_as_string()
-
-#print(tfstate)
-# deserialized json
-data = json.loads(tfstate)
-print(data['modules'])
+        print json.dumps(self.inventory);
+                            # write ro inventory.json
+       	
 
 
+    # Example inventory for testing.
+    def ansible_inventory(self):
+    	#read bucket
+		client = storage.Client()
+		bucket = client.get_bucket(gcp_bucket)
+		blob = bucket.get_blob(bucket_folder + '/default.tfstate')
+		tfstate = blob.download_as_string()
+
+		#print(tfstate)
+		# deserialized json
+		data = json.loads(tfstate)
+		#print(data['modules'])
+
+		tf_modules = data['modules']
+		# print(tf_modules)
+
+		group_app_hosts = []
+		group_db_hosts = []
+		
+
+		for k in tf_modules:
+			if k['resources']:
+				for key,val in k['resources'].items():
+					if re.match('google_compute_instance',key):
+						#print(val['primary']['attributes'])
+						if re.search('app',val['primary']['id']):
+							group_app_hosts.append(val['primary']['attributes']['network_interface.0.access_config.0.nat_ip'])
+						if re.search('db',val['primary']['id']):
+							group_db_hosts.append(val['primary']['attributes']['network_interface.0.access_config.0.nat_ip'])
+						
+
+		inventory = {}
+		if group_app_hosts:
+			inventory['app'] = {}
+			inventory['app']['hosts'] = group_app_hosts
+		if group_db_hosts:
+			inventory['db'] = {}
+			inventory['db']['hosts'] = group_db_hosts
+		inventory.update({'_meta':{'hostvars':{}}})
+		#print inventory
+		return inventory
+        # return {
+        #     'app': {
+        #         'hosts': ['192.168.28.71', '192.168.28.72']
+        #     },
+        #     'db': {
+        #     	'hosts': []
+        #     },
+        #     '_meta': {
+        #         'hostvars': {
+        #             '192.168.28.71': {
+        #                 'host_specific_var': 'foo'
+        #             },
+        #             '192.168.28.72': {
+        #                 'host_specific_var': 'bar'
+        #             }
+        #         }
+        #     }
+        # }
+
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+# Get the inventory.
+DynamicInventory()
